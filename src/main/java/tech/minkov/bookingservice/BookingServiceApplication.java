12ea8a37b74481ca67643a1f6bcc9fdd3dab8b6f
Atanas Minkov <atanas.minkov@icloud.com>
2020-03-05 04:26:04 +0200

    CORS config

--- src/main/java/tech/minkov/bookingservice/BookingServiceApplication.java
+++ src/main/java/tech/minkov/bookingservice/BookingServiceApplication.java
@@ -2,7 +2,10 @@ package tech.minkov.bookingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -10,5 +13,15 @@ public class BookingServiceApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(BookingServiceApplication.class, args);
+  }
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/graphql").allowedOrigins("http://localhost:4200");
+      }
+    };
   }
 }
